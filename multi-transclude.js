1b90ae87aa98820d96094383f210e87d8a90af0b
Paolo Scanferla <paolo.scanferla@gmail.com>
2014-04-07 16:51:50 +0200

    Initial commit

--- multi-transclude.js
+++ multi-transclude.js
@@ -4,12 +4,12 @@ angular.module("mnd.multi-transclude", [])
 	return {
 		link: function ($scope, $element, $attrs, controller, $transclude) {
 			if (!$transclude) {
-				throw new Error("Illegal use of psMultiTransculde");
+				throw new Error("Illegal use of mndMultiTransculde");
 			}
 			$transclude(function (clone) {
 				var wrapper = angular.element("<div></div>");
 				wrapper.append(clone);
-				var selector = "[name=" + $attrs.psMultiTransclude + "]";
+				var selector = "[name=" + $attrs.mndMultiTransclude + "]";
 				var target = wrapper[0].querySelector(selector);
 				target = angular.element(target);
 				$element.replaceWith(target);
